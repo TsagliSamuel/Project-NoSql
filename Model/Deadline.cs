846fa6266d216ef4789828f51b1f472b0e92b0f5
TsagliSamuel <54842837+TsagliSamuel@users.noreply.github.com>
2020-11-01 18:41:22 +0100

    Add files via upload

---
+++ Model/Deadline.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Model
+{
+    public class Deadline //not sure 
+    {
+        public string days { get; set; } //should be readonly //testing
+        public TimeSpan timespan { get; set; } //should be reaonly //testing
+
+        public static Deadline week1 = new Deadline("14 days", new TimeSpan(14, 0, 0, 0)); //set a static deadline field 
+        public static List<Deadline> deadlines = new List<Deadline>() { week1 }; //add it to the list of deadlines (to be populated later)
+        
+        public Deadline(string days,TimeSpan timespan) 
+        {
+            this.days = days;
+            this.timespan = timespan;
+        }
+        public static DateTime setDeadline(DateTime reportedDate,TimeSpan timespan) //set deadline of timespan 14 days 
+        {
+            if(timespan.Days == 14)
+            {
+                return reportedDate.AddDays(14);
+            }
+            return reportedDate;
+        }
+    }
+}
---
+++ Model/Incident.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MongoDB.Bson;
+
+namespace Model
+{
+    public class Incident
+    {
+        public ObjectId id;
+        public Status status;
+        public Priority priority;
+        public DateTime deadline;
+        public IncidentType incidentType;
+        public ObjectId userReported;
+        public string description;
+        public DateTime reportedDate;
+        public string subject;
+
+       
+        public Incident() { }
+    }
+
+   
+}
