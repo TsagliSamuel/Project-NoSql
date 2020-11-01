78504b53bafb5bd6a3d4faf4284542d5897f562c
TsagliSamuel <54842837+TsagliSamuel@users.noreply.github.com>
2020-11-01 18:37:28 +0100

    Add files via upload

---
+++ DAL/Base.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MongoDB.Driver;
+using MongoDB.Bson;
+
+namespace DAL
+{
+    public abstract class Base
+    {
+        protected MongoClient dbClient;
+        protected IMongoDatabase database;        
+        protected string incidentsCollection = "Incidents"; 
+
+
+        public Base()
+        {
+            dbClient = new MongoClient("mongodb+srv://Group1DB:group1database@cluster0.ymqob.mongodb.net/Group1DB?retryWrites=true&w=majority");
+            database = dbClient.GetDatabase("Group1DB");
+            
+        }
+        protected List<BsonDocument> GetAll(string collections)
+        {
+            var collection = database.GetCollection<BsonDocument>(collections);
+            var emptyFilter = Builders<BsonDocument>.Filter.Empty; //empty filter to get all documents
+            var docs = collection.Find(emptyFilter).ToList();
+            return docs;
+        }
+        protected List<BsonDocument> GetSpecificIncidents(string collections,string attribute,string value) //getting incidents by string 
+        {
+
+            var collection = database.GetCollection<BsonDocument>(collections);
+            var filterEq = Builders<BsonDocument>.Filter.Eq(attribute, value);
+            var docs = collection.Find(filterEq).ToList();
+            return docs;
+        }
+        protected List<BsonDocument> GetSpecificIncidents(string collections, FilterDefinition<BsonDocument> filteredDocument) //filtering incidents by filterDefinition in docs
+        {
+            var collection = database.GetCollection<BsonDocument>(collections);
+            var docs  = collection.Find(filteredDocument).ToList();
+            return docs;
+        }
+
+        
+    }
+}
---
+++ DAL/IncidentDAO.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using MongoDB.Driver;
+using MongoDB.Bson;
+using Model;
+
+namespace DAL
+{
+    public class IncidentDAO : Base
+    {
+        public List<Incident> GetIncidents(List<BsonDocument> bsonDocument)
+        {
+            List<Incident> incidents = new List<Incident>();
+
+            foreach (var i in bsonDocument)
+            {
+                Incident incident = new Incident()
+                {
+                    id = (ObjectId)i["_id"],
+                    status = (Status)Enum.Parse(typeof(Status), i["Status"].ToString()),
+                    priority = (Priority)Enum.Parse(typeof(Priority), i["Priority"].ToString()),
+                    deadline = DateTime.Parse(i["Deadline"].ToString()),
+                    incidentType = (IncidentType)Enum.Parse(typeof(IncidentType), i["IncidentType"].ToString()),
+                    userReported = (ObjectId)i["UserReported"],
+                    description = (string)i["Description"],
+                    reportedDate = DateTime.Parse(i["ReportedDate"].ToString()),
+                    subject = (string)i["Subject"]
+                };
+                incidents.Add(incident);
+            }
+            return incidents;
+        }
+        public List<Incident> GetAllIncidents(FilterDefinition<BsonDocument> filteredDocuments) 
+        { 
+            return GetIncidents(GetSpecificIncidents(incidentsCollection, filteredDocuments)); 
+        }
+
+        public List<Incident> GetAllIncidents()
+        {
+            return GetIncidents(GetAll(incidentsCollection));
+        }
+
+        public List<Incident> GetSpecificIncidents(string status, string column)
+        {
+            return GetIncidents(GetSpecificIncidents(incidentsCollection, status, column));
+        }
+
+       
+
+
+    }
+}
